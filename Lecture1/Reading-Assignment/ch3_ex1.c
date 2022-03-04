34efd0d067d10035cc3a140ee033635709e42c48
CJ <47445339+xkaze09@users.noreply.github.com>
2022-03-04 14:20:43 +0800

    Add ch3_ex1.c

---
+++ Lecture1/Reading-Assignment/ch3_ex1.c
@@ -0,0 +1,17 @@
+// For printf function
+#include <stdio.h>
+
+int main() {
+    // Variable Declaration and Initialization
+    // using int and float data types
+    int i, j;
+    float x, y;
+    i = 10;
+    j = 20;
+    x = 43.2892f;
+    y = 5527.0f;
+    // End of Variable Declaration and Initialization
+
+    // Display output by format string
+    printf("i = %d, j = %d, x = %f, y = %f\n", i, j, x, y);
+}
